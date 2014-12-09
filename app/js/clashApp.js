7fd9082d4f7d3ebdc30a90621f5ff59e3f5b4b32
gug328 <joshua.white@capitalone.com>
2014-12-09 11:10:39 -0800

    Adding a bunch of json files for buildings, so basic flow...

--- app/js/clashApp.js
+++ app/js/clashApp.js
@@ -1,5 +1,4 @@
 // Javascript file for Clash building planner
-
 var clashApp = angular.module('clashApp', ['ngRoute']);
 
 // Service definiton
@@ -18,14 +17,24 @@ clashApp.service("ResourceService", function() {
 // Controller definition
 var controllers = {};
 controllers.goldController =  function($scope, $http) {
-	$http.get('buildings/buildings.json')
-		.success(function(data, status) {
-			$scope.buildings = data;
-		});
-
 	$scope.title = "Structures for Gold";
 
-	$scope.structures = [
+	$http.get('buildings/buildings.json')
+		.success(function(all_buildings) {
+			var buildings = [];
+
+			for (i=0; i < all_buildings[0].buildings.length; i++) {
+				$http.get('buildings/'+all_buildings[0].buildings[i].id+'.json')
+					.success(function(detail_info) {
+						buildings.push(detail_info);
+					});
+			}
+
+			$scope.buildings = buildings;
+		});
+
+	$scope.structures =
+	[
 		{ building:'Archer Tower', level:4, cost:1500000, time:4, howMany:4 },
 		{ building:'Cannon', level:9, cost:800000, time:3, howMany:5 }
 	];
